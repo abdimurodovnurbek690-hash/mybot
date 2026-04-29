7e39c64338ff070c3c1eb88c513ed5a802717a4e
abdimurodovnurbek690-hash <abdimurodovnurbek690@gmail.com>
2026-04-29 13:06:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,50 +2,67 @@ import os
 import telebot
 from telebot import types
 
-# Railway environment variable
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
+
+user_data = {}
 
 # START
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn = types.KeyboardButton("📦 Buyurtma berish")
-    markup.add(btn)
+    markup.add("📦 Buyurtma berish")
 
     bot.send_message(message.chat.id, "Assalomu alaykum! Buyurtma berish uchun tugmani bosing.", reply_markup=markup)
 
-# BUYURTMA
+
+# BUYURTMA BOSILDI
 @bot.message_handler(func=lambda message: message.text == "📦 Buyurtma berish")
 def order(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("🍎 Olma", "🍌 Banan")
+
+    bot.send_message(message.chat.id, "Mahsulotni tanlang:", reply_markup=markup)
+
+
+# MAHSULOT TANLASH
+@bot.message_handler(func=lambda message: message.text in ["🍎 Olma", "🍌 Banan"])
+def product(message):
+    user_data[message.chat.id] = {"product": message.text}
+
     btn = types.KeyboardButton("📱 Telefon raqam yuborish", request_contact=True)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(btn)
 
     bot.send_message(message.chat.id, "Telefon raqamingizni yuboring:", reply_markup=markup)
 
-# CONTACT
+
+# TELEFON
 @bot.message_handler(content_types=['contact'])
 def contact(message):
-    phone = message.contact.phone_number
+    user_data[message.chat.id]["phone"] = message.contact.phone_number
 
-    bot.send_message(message.chat.id, "📍 Manzil yuboring yoki yozing (masalan: Chilonzor)")
+    bot.send_message(message.chat.id, "📍 Manzil yuboring yoki yozing:")
 
-    bot.register_next_step_handler(message, get_location, phone)
 
-def get_location(message, phone):
-    location = message.text
+# MANZIL
+@bot.message_handler(func=lambda message: True)
+def location(message):
+    data = user_data.get(message.chat.id, {})
 
-    text = f"""
-📦 Yangi buyurtma:
-📱 {phone}
-📍 {location}
-"""
+    product = data.get("product", "Noma'lum")
+    phone = data.get("phone", "Noma'lum")
+    address = message.text
 
-    bot.send_message(message.chat.id, text)
+    bot.send_message(message.chat.id,
+        f"📦 Yangi buyurtma:\n"
+        f"{product}\n"
+        f"📞 {phone}\n"
+        f"📍 Manzil: {address}"
+    )
+
     bot.send_message(message.chat.id, "✅ Buyurtma qabul qilindi!")
 
-# RUN
-print("Bot ishlayapti...")
+
 bot.infinity_polling()
